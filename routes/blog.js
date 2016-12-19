aef52bff64f0fcb2a61957ee081f49dfa35672b9
IGoRFonin <menotoa@gmail.com>
2016-12-19 23:13:37 +0300

    change ViewController to blog router

--- routes/blog.js
+++ routes/blog.js
@@ -6,24 +6,28 @@ const ViewController = require('../controller/ViewController');
 var router = express.Router();
 router.get('/', function(req, res, next) {
 	BlogController.getAll({'sort':'asc'}, function(err, posts) {
-		let view = new ViewController('pages/blog/index', res);
-		let data = {};
+		let view = new ViewController('pages/blog/index', res),
+				data = {};
+
 		data.posts = posts;
 		data.title = "Блог";
 		for(let post in posts) {
 			posts[post].link = "/blog/" + posts[post].code;
 		}
-		// res.render('pages/blog/index', data);
+		
 		view.render(data);
 	});
 });
 
 router.get('/:code', (req, res, next) => {
 	BlogController.getPost( req.params.code, function(err, post) {
-		let data = {};
+		let view = new ViewController('pages/blog/detail', res),
+				data = {};
+
 		data.title = post.name;
 		data.post = post;
-		res.render('pages/blog/detail', data);
+		
+		view.render(data);
 	})
 })
 
